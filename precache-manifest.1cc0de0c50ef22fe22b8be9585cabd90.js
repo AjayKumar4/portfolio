9d9ba86007e09e474710f04c2de9be55352e3793
AJ <n.uajaykumar@gmail.com>
2018-10-30 00:57:49 +0000

    Updates

---
+++ precache-manifest.1cc0de0c50ef22fe22b8be9585cabd90.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "56aa79c13096e30dc2f09a7d0118c2b2",
+    "url": "/portfolio/static/media/github.56aa79c1.png"
+  },
+  {
+    "revision": "1611318877ab45d3cc6c",
+    "url": "/portfolio/static/css/main.75752de9.chunk.css"
+  },
+  {
+    "revision": "91696e0984cfab74ff344325e5015da5",
+    "url": "/portfolio/static/media/Montserrat-Regular.91696e09.otf"
+  },
+  {
+    "revision": "1ebc759754d3c19fa9c6",
+    "url": "/portfolio/static/js/1.1ebc7597.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f6eaf68b925e626e37d1815b383e8a1d",
+    "url": "/portfolio/static/media/linkedin.f6eaf68b.jpg"
+  },
+  {
+    "revision": "1611318877ab45d3cc6c",
+    "url": "/portfolio/static/js/main.16113188.chunk.js"
+  },
+  {
+    "revision": "9f22e533a3a020fe569b9aa9f8529028",
+    "url": "/portfolio/static/media/creative.9f22e533.png"
+  },
+  {
+    "revision": "62670766877bd8790c0eb5bfd7063e2b",
+    "url": "/portfolio/static/media/epcet-logo.62670766.png"
+  },
+  {
+    "revision": "a1e6da603a5d0e60f93a046f5dae5ded",
+    "url": "/portfolio/static/media/spiderlogic-logo.a1e6da60.png"
+  },
+  {
+    "revision": "1ebc759754d3c19fa9c6",
+    "url": "/portfolio/static/css/1.0b2146e5.chunk.css"
+  },
+  {
+    "revision": "00bb9f799c39d58f4d6938d4ba3927b4",
+    "url": "/portfolio/index.html"
+  }
+];
