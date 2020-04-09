7f0789779c820157857fb1add0f736825e1e62eb
AJ <ajay.kumar@siren.io>
2020-04-09 07:57:22 +0100

    init react setup

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
-import React, { Component } from 'react';
-import '../node_modules/materialize-css/dist/css/materialize.min.css';
-import '../node_modules/materialize-css/dist/js/materialize.min.js';
-
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-//import './App.scss';
-//import './main.scss';
 
-import NavBar from './components/navbar';
-import Home from './components/landing_page';
-//import Work from './components/work';
-import About from './components/about';
-import Skills from './components/skills';
-//import Contact from './components/contact';
-import Footer from './components/footer';
-
-class App extends Component {
-  /*render() {
-    return (
-      <div className="portfolio">
-        <NavBar/>
-        <Home/>
-		<About/>
-		<Skills/>
-        <Work/>
-        <Contact/>
-        <Footer/>
-      </div>
-    );
-  }*/
-  render() {
-    return (
-      <div className="portfolio">
-        <NavBar/>
-        <Home/>
-        <About/>
-        <Skills/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
